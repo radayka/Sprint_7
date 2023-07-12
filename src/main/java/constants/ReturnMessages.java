865bcb1c219c1b8565b6f3e70b912d6977265974
Max Radaev <maksimka.radaev@inbox.ru>
2023-07-12 12:03:55 +0300

    -added Tests

--- src/main/java/constants/ReturnMessages.java
+++
@@ -1,8 +0,0 @@
-package constants;
-
-public class ReturnMessages {
-    public final static String NOT_ENOUGH_DATA_CREATE = "Недостаточно данных для создания учетной записи";
-    public final static String CONFLICT = "Этот логин уже используется. Попробуйте другой.";
-    public final static String NOT_ENOUGH_DATA_LOGIN = "Недостаточно данных для входа";
-    public final static String NOT_FOUND = "Учетная запись не найдена";
-}
